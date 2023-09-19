7787ead76cb7a434302196dd3bb92486eacb3edd
Rfaelsn <rafaelnascimentovf6@gmail.com>
2023-09-19 12:29:35 -0300

    feat: realizando configuração de icones

--- src/components/menu-options/menu-options.vue
+++ src/components/menu-options/menu-options.vue
@@ -1,34 +1,116 @@
 <template>
-    <div>
-        <nav class="option-nav">
-            <ul>
-                <li>
-                    <p>Inicio de Estagio</p>
-                    <div class="expansive-options">
-                        <RouterLink to="/estagio/obrigatorio">Obrigatório</RouterLink>
-                        <RouterLink to="/estagio/naoobrigatorio">Não Obrigatório</RouterLink>
-                    </div>
-                </li>
-                <li>
-                    <p>Fim de Estágio</p>
-                    <div class="expansive-options">
-                        <RouterLink to="/estagio/obrigatorio">Obrigatório</RouterLink>
-                        <RouterLink to="/estagio/naoobrigatorio">Não Obrigatório</RouterLink>
-                    </div>
-                </li>
-                <li>
-                    <p>Acompanhar Processos</p>
-                </li>
-                <li>
-                    <p>Configurações</p>
-                </li>
-        </ul>
-        </nav>
-    </div>
+     <v-card
+    class="mx-auto"
+    width="300"
+  >
+    <v-list v-model:opened="open">
+      <v-list-item prepend-icon="mdi-home" title="Home"></v-list-item>
+
+      <v-list-group value="Users">
+        <template v-slot:activator="{ props }">
+          <v-list-item
+            v-bind="props"
+            prepend-icon="mdi-account-circle"
+            title="Users"
+          ></v-list-item>
+        </template>
+
+        <v-list-group value="Admin">
+          <template v-slot:activator="{ props }">
+            <v-list-item
+              v-bind="props"
+              title="Admin"
+            ></v-list-item>
+          </template>
+
+          <v-list-item
+            v-for="([title, icon], i) in admins"
+            :key="i"
+            :title="title"
+            :prepend-icon="icon"
+            :value="title"
+          ></v-list-item>
+        </v-list-group>
+
+        <v-list-group value="Actions">
+          <template v-slot:activator="{ props }">
+            <v-list-item
+              v-bind="props"
+              title="Actions"
+            ></v-list-item>
+          </template>
+
+          <v-list-item
+            v-for="([title, icon], i) in cruds"
+            :key="i"
+            :value="title"
+            :title="title"
+            :prepend-icon="icon"
+          ></v-list-item>
+        </v-list-group>
+      </v-list-group>
+
+      <v-list-group value="Users">
+        <template v-slot:activator="{ props }">
+          <v-list-item
+            v-bind="props"
+            prepend-icon="mdi-account-circle"
+            title="Users"
+          ></v-list-item>
+        </template>
+
+        <v-list-group value="Admin">
+          <template v-slot:activator="{ props }">
+            <v-list-item
+              v-bind="props"
+              title="Admin"
+            ></v-list-item>
+          </template>
+
+          <v-list-item
+            v-for="([title, icon], i) in admins"
+            :key="i"
+            :title="title"
+            :prepend-icon="icon"
+            :value="title"
+          ></v-list-item>
+        </v-list-group>
+
+        <v-list-group value="Actions">
+          <template v-slot:activator="{ props }">
+            <v-list-item
+              v-bind="props"
+              title="Actions"
+            ></v-list-item>
+          </template>
+
+          <v-list-item
+            v-for="([title, icon], i) in cruds"
+            :key="i"
+            :value="title"
+            :title="title"
+            :prepend-icon="icon"
+          ></v-list-item>
+        </v-list-group>
+      </v-list-group>
+    </v-list>
+  </v-card>
 </template>
 
 <script lang="ts" setup>
+    import { ref } from 'vue';
 
+const open = ref(['Users']);
+const admins = ref([
+  ['Management', 'mdi-account-multiple-outline'],
+  ['Settings', 'mdi-cog-outline'],
+]);
+const cruds = ref([
+  ['Create', 'mdi-plus-outline'],
+  ['Read', 'mdi-file-outline'],
+  ['Update', 'mdi-update'],
+  ['Delete', 'mdi-delete'],
+]);
 </script>
 
 <style src="./style.scss" lang="scss" scoped>
--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import App from './App.vue'
 import router from './router/router'
 
 // Vuetify
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
