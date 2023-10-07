360e935c66e85dcc650a40abee304ab5c03fb154
Rfaelsn <rafaelnascimentovf6@gmail.com>
2023-10-07 02:16:51 -0300

    feat: implementando data-table de acompanhamento de processos

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,7 +1,17 @@
-import { createVuetify } from 'vuetify'
-import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import { createVuetify } from 'vuetify';
+import { aliases, mdi } from 'vuetify/iconsets/mdi';
+import {
+  VDataTable,
+  VDataTableServer,
+  VDataTableVirtual,
+} from "vuetify/labs/VDataTable";
 
 export default createVuetify({
+  components: {
+    VDataTable,
+    VDataTableServer,
+    VDataTableVirtual,
+  },
   icons: {
     defaultSet: 'mdi',
     aliases,
--- src/views/acompanhar-processos/visao-dex/acompanhar-processos.vue
+++ src/views/acompanhar-processos/visao-dex/acompanhar-processos.vue
@@ -1,12 +1,26 @@
 <template>
-   <div>
-
-   </div>
+  <div>
+    <div class="title">
+      <h1 class="title1">Grid de Processos</h1>
+    </div>
+    <DataTable></DataTable>
+  </div>
 </template>
 
 <script setup lang="ts">
+  import  DataTable  from '../../../components/data-table/data-table.vue'
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+.title1 {
+text-align: left;
+color: #858796;
+font-size: 1.5rem;
+margin-bottom: 1%;
+}
 </style>
+
+
+
+
+
