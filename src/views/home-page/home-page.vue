faf7e79afcab9b5e418d96764598a9c2f4faf6be
Rfaelsn <rafaelnascimentovf6@gmail.com>
2023-10-19 11:48:07 -0300

    feat[home]: implementando carousel de imagens

--- src/views/home-page/home-page.vue
+++ src/views/home-page/home-page.vue
@@ -1,45 +1,37 @@
 <template>
-  <div>
-    <v-carousel
+  <div class="teste">
+    <v-carousel :show-arrows="false"
     cycle
-    height="400"
+    height="300"
     hide-delimiter-background
-    show-arrows="hover"
+    hide-delimiters
+    
   >
-    <v-carousel-item
-      v-for="(slide, i) in slides"
+  <v-carousel-item
+      v-for="(item,i) in items"
       :key="i"
-    >
-      <v-sheet
-        :color="colors[i]"
-        height="100%"
-      >
-        <div class="d-flex fill-height justify-center align-center">
-          <div class="text-h2">
-            {{ slide }} Slide
-          </div>
-        </div>
-      </v-sheet>
-    </v-carousel-item>
+      :src="item.src"
+      cover
+    ></v-carousel-item>
   </v-carousel>
   </div>  
 </template>
 
 <script lang="ts" setup>
-  const colors = [
-          'indigo',
-          'warning',
-          'pink darken-2',
-          'red lighten-1',
-          'deep-purple accent-4',
-        ]
-  const slides = [
-  'First',
-          'Second',
-          'Third',
-          'Fourth',
-          'Fifth',
-  ]
+  const items = [
+    {
+      src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+    },
+    {
+      src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+    },
+    {
+      src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+    },
+    {
+      src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+    },
+    ]
 </script>
 
 <style src="./style.scss" lang="scss" scoped>
